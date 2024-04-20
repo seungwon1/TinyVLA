f5549e8f38d8bed6945516f20378f9536d0d4703
3D ALGO LAB <threealgo@outlook.com>
2024-04-20 19:47:04 +0900

    vectorized

--- simul/a2c_train.py
+++ simul/a2c_train.py
@@ -1,9 +1,41 @@
 import gymnasium as gym
 import panda_gym  # noqa: F401
 from stable_baselines3 import A2C
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 
-env = gym.make("PandaReachDense-v3")
-model = A2C(policy="MultiInputPolicy", env=env, verbose=1)
 
-model.learn(total_timesteps=1_000_000, progress_bar=True)
-model.save("data/a2c_panda_reach")
+def make_env(env_id: str, rank: int, seed: int = 0):
+    """
+    Utility function for multiprocessed env.
+
+    :param env_id: the environment ID
+    :param num_env: the number of environments you wish to have in subprocesses
+    :param seed: the inital seed for RNG
+    :param rank: index of the subprocess
+    """
+
+    def _init():
+        env = gym.make(env_id, render_mode="human")
+        env.reset(seed=seed + rank)
+        return env
+
+    set_random_seed(seed)
+    return _init
+
+
+if __name__ == "__main__":
+    env_id = "PandaReachDense-v3"
+    num_cpu = 6
+
+    env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
+    model = A2C(policy="MultiInputPolicy", env=env, verbose=1)
+    model.learn(total_timesteps=1_000_000, progress_bar=True)
+    model.save("data/a2c_panda_reach")
+
+    obs, info = env.reset()
+    for _ in range(1000):
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
