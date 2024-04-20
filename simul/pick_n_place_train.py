7b9c1dd7f8855f0a668bef4a90e8c15c6a1741e4
3D ALGO LAB <threealgo@outlook.com>
2024-04-20 23:54:57 +0900

    update pick and place simul.

--- simul/pick_n_place_train.py
+++ simul/pick_n_place_train.py
@@ -2,12 +2,14 @@ import panda_gym  # noqa: F401
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import SubprocVecEnv
+import multiprocessing as mp
 
-num_cpu = 10
+num_cpu = mp.cpu_count()
 env_id = "PandaPickAndPlace-v3"
 
 if __name__ == "__main__":
+    print("cpu count:", num_cpu)
     env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
     model = PPO(policy="MultiInputPolicy", env=env, verbose=1)
-    model.learn(total_timesteps=1_000_000, progress_bar=True)
+    model.learn(total_timesteps=50_000_000, progress_bar=True)
     model.save("data/ppo_panda_pick_and_place")
