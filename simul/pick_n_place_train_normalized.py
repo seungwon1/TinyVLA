c053aa63ffb27ee2115258159b8b4289c40f9c99
3D ALGO LAB <threealgo@outlook.com>
2024-04-21 13:58:26 +0900

    vsft for LLaVA training added

--- simul/pick_n_place_train_normalized.py
+++ simul/pick_n_place_train_normalized.py
@@ -1,8 +1,9 @@
+import multiprocessing as mp
+
 import panda_gym  # noqa: F401
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize
-import multiprocessing as mp
 
 num_cpu = mp.cpu_count()
 env_id = "PandaPickAndPlace-v3"
@@ -12,6 +13,6 @@ if __name__ == "__main__":
     env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.0)
 
     model = PPO(policy="MultiInputPolicy", env=env, verbose=1)
-    model.learn(total_timesteps=1_000_000, progress_bar=True)
+    model.learn(total_timesteps=10_000_000, progress_bar=True)
     model.save("data/ppo_panda_pick_and_place")
     env.save("data/ppo_panda_pick_and_place_env.pkl")
